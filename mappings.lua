365dabe4a67ef6adae1600641471c49eaf048a51
eric <eirtob98@gmail.com>
2023-04-11 02:46:06 +0200

    update config

--- mappings.lua
+++ mappings.lua
@@ -15,9 +15,47 @@ return {
       end,
       desc = "Pick to close",
     },
+    ["j"] = {
+      function() vim.lsp.buf.hover() end,
+      desc = "Hover symbol details",
+    }, 
+    ["n"] = "h",
+    ["e"] = "j",
+    ["i"] = "k",
+    ["o"] = "l",
+    ["s"] = "i",
+    ["t"] = "o",
+    ["h"] = { 
+      function()
+        require("leap").leap { offset = 1 }
+      end,
+      desc = "Leap forward"
+    },
+    ["H"] = { 
+      function()
+        require("leap").leap { offset = 1, backward = true }
+      end,
+      desc = "Leap forward"
+    },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>wh"] = {
+      "<cmd>split<cr>",
+      desc = "Horizontal split buffer from tabline",
+    },
+    ["<leader>wv"] = {
+      "<cmd>vsplit<cr>",
+      desc = "Vertical split buffer from tabline",
+    },
+    ["<leader>wn"] = { "<cmd>wincmd h<cr>", desc = "Move to left split" },
+    ["<leader>we"] = { "<cmd>wincmd j<cr>", desc = "Move to down split" },
+    ["<leader>wi"] = { "<cmd>wincmd k<cr>", desc = "Move to up split" },
+    ["<leader>wo"] = { "<cmd>wincmd l<cr>", desc = "Move to right split" },
+    ["ga"] = {
+      function() vim.lsp.buf.code_action() end,
+      desc = "LSP code action",
+    }
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
@@ -25,4 +63,22 @@ return {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
+  v = {
+    ["n"] = "h",
+    ["e"] = "j",
+    ["i"] = "k",
+    ["o"] = "l",
+    ["h"] = { 
+      function()
+        require("leap").leap { offset = 1 }
+      end,
+      desc = "Leap forward"
+    },
+    ["H"] = { 
+      function()
+        require("leap").leap { offset = 1, backward = true }
+      end,
+      desc = "Leap forward"
+    },
+  }
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,30 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  "andweeb/presence.nvim",
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp_signature").setup()
+    end,
+  },
+  {
+    "ggandor/leap.nvim",
+    config = function()
+      require('leap').opts.safe_labels = { 'h', 'f', 'l', 'u', 't' }
+      require('leap').opts.labels = { 'h', 'f', 'l', 'u', 't' }
+    end,
+  },
+  {
+    "kwkarlwang/bufjump.nvim",
+      config = function()
+        require("bufjump").setup({
+            forward = "<leader>o",
+            backward = "<leader>n",
+            on_success = nil
+        })
+    end,
+  },
+  'folke/tokyonight.nvim',
 }
