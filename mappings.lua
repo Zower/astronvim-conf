66e055fcccdf6b821d400f13ca5ee0dbe3f720d7
eric <eirtob98@gmail.com>
2023-04-11 14:25:15 +0200

    update config

--- mappings.lua
+++ mappings.lua
@@ -40,22 +40,43 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    ["<leader>wh"] = {
+    ["<leader>bh"] = {
       "<cmd>split<cr>",
       desc = "Horizontal split buffer from tabline",
     },
-    ["<leader>wv"] = {
+    ["<leader>bv"] = {
       "<cmd>vsplit<cr>",
       desc = "Vertical split buffer from tabline",
     },
-    ["<leader>wn"] = { "<cmd>wincmd h<cr>", desc = "Move to left split" },
-    ["<leader>we"] = { "<cmd>wincmd j<cr>", desc = "Move to down split" },
-    ["<leader>wi"] = { "<cmd>wincmd k<cr>", desc = "Move to up split" },
-    ["<leader>wo"] = { "<cmd>wincmd l<cr>", desc = "Move to right split" },
+    ["<leader>bn"] = { "<cmd>wincmd h<cr>", desc = "Move to left split" },
+    ["<leader>be"] = { "<cmd>wincmd j<cr>", desc = "Move to down split" },
+    ["<leader>bi"] = { "<cmd>wincmd k<cr>", desc = "Move to up split" },
+    ["<leader>bo"] = { "<cmd>wincmd l<cr>", desc = "Move to right split" },
+    ["<leader>bq"] = { "<cmd>close<cr>", desc = "Close window" },
     ["ga"] = {
       function() vim.lsp.buf.code_action() end,
       desc = "LSP code action",
-    }
+    },
+    ["<leader>n"] = { "<c-o>", desc = "Jump back" },
+    ["<leader>o"] = { "<c-i>", desc = "Jump forward" },
+    ["<c-f>"] = { function() require("telescope.builtin").live_grep() end, desc = "Find words" },
+    ["<leader>e"] = { function() require("telescope.builtin").oldfiles() end, desc = "Find history" },
+    ["<leader>i"] = { function() require("telescope.builtin").find_files() end, desc = "Find files" },
+    ["<leader>fo"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
+    ["<leader>ff"] = {
+      function()
+        if vim.bo.filetype == "neo-tree" then
+          vim.cmd.wincmd "p"
+        else
+          vim.cmd.Neotree "focus"
+        end
+      end,
+      desc = "Toggle Explorer Focus",
+    },
+    ["<leader>r"] = {
+      function() vim.lsp.buf.rename() end,
+      desc = "Rename current symbol",
+    },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
--- plugins/user.lua
+++ plugins/user.lua
@@ -27,4 +27,17 @@ return {
     end,
   },
   'folke/tokyonight.nvim',
+  {
+    'akinsho/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = function()
+      require("flutter-tools").setup {
+        autostart = true
+      }
+    end
+  }
 }
